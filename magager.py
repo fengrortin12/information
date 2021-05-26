b69d3cf130f6ae9a7dd05c673343665ee0725588
fengrortin12 <yiming1988f@126.com>
2021-05-26 11:34:01 +0800

    项目配置&数据库配置

--- magager.py
+++ magager.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目配置"""
+    DEBUG = True
+
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information2"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+# 初始化数据库
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -10,4 +24,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
